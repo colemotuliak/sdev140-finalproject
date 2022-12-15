979adfff7fcefa14edd45762e8b1b8b112f9cf78
Cole Motuliak <21240755+colemotuliak@users.noreply.github.com>
2022-12-14 22:15:21 -0500

    working GUI

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 '''
 Program Name: SNMPWidget 2000
 Author: Cole Motuliak
@@ -7,48 +8,73 @@ Purpose: A program to perform SNMP GET requests.
 # Import the high level API from PySNMP
 from pysnmp.hlapi import *
 
-# temporary constants for testing
-COMMUNITY_STRING = "T3$tROsoit"
-HOST = "172.16.253.13"
-PORT = 161
+# Import EasyFrame from BreezyPythonGUI
+from breezypythongui import EasyFrame
+
 SYSNAME_OID = "1.3.6.1.2.1.1.5.0"
 SYSDESCR_OID = "1.3.6.1.2.1.1.1.0"
-# define the Get module to perform an SNMP GET request and print the value
-def Get(community,host,port,oid):
 
-    # build the get object using the snmp engine from the PySNMP Hi-level API
-    # mpModel =1 means we are using SNMPv2c
-    getObject = getCmd(
-        SnmpEngine(),
-        CommunityData(community, mpModel=1),
-        UdpTransportTarget((host, port)),
-        ContextData(),
-        ObjectType(ObjectIdentity(oid))
-    )
 
-    # set error check variables to the getobject
-    errorIndication, errorStatus, errorIndex, varBinds = next(getObject)
 
-    # if an error exists, print it
-    if errorIndication:
-        print(errorIndication)
 
-    elif errorStatus:
-        print('%s at %s' % (errorStatus.prettyPrint(),
-                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+class SNMPGet(EasyFrame):
+ 
+    def __init__(self):
+        # set up the window and all of the buttons/fields
+        EasyFrame.__init__(self, title = "SNMPWidget 2000")
+  
+        # Label and field for the community string
+        self.addLabel(text = "Community String", row = 0, column = 0)
+        self.communityField = self.addTextField(text = "", row = 0, column = 1, width = 20)
+ 
+        # Label and field for the device IP address
+        self.addLabel(text = "Device IP address", row = 1, column = 0)
+        self.ipField = self.addTextField(text = "", row = 1, column = 1, width = 20)
+  
+        # Label and field for the object identifier
+        self.addLabel(text = "OID", row = 2, column = 0)
+        self.oidField = self.addTextField(text = "", row = 2, column = 1)
 
-    # if no error, print the response
-    else:
-        for varBind in varBinds:
-            print(' = '.join([x.prettyPrint() for x in varBind]))
+        # The command button
+        self.addButton(text = "Get", row = 3, column = 0, columnspan = 2, command = self.snmpGet)
 
-# Run it
-print("Print a couple OIDs")
-Get(COMMUNITY_STRING,HOST,PORT,SYSNAME_OID)
-Get(COMMUNITY_STRING,HOST,PORT,SYSDESCR_OID)
+        # Label and field for the output
+        self.addLabel(text = "Output", row = 4, column = 0)
+        self.responseField = self.addTextArea(text = "", row = 4, column = 1)
+  
 
-print("Prompt the user")
+    def snmpGet(self):
 
-while True:
-    oid = input("Enter an OID: ")
-    Get(COMMUNITY_STRING,HOST,PORT,oid)
+        # build the get object using the snmp engine from the PySNMP Hi-level API
+        # mpModel =1 means we are using SNMPv2c
+        community = self.communityField.getText()
+        port = 161
+        host = self.ipField.getText()
+        oid = self.oidField.getText()
+        getObject = getCmd(
+            SnmpEngine(),
+            CommunityData(community, mpModel=1),
+            UdpTransportTarget((host, port)),
+            ContextData(),
+            ObjectType(ObjectIdentity(oid))
+        )
+
+        # set error check variables to the getobject
+        errorIndication, errorStatus, errorIndex, varBinds = next(getObject)
+
+        # if an error exists, print it
+        if errorIndication:
+            print(errorIndication)
+
+        elif errorStatus:
+            print('%s at %s' % (errorStatus.prettyPrint(),
+                                errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+
+        # if no error, print the response
+        else:
+            for varBind in varBinds:
+                self.responseField.setText( (' = '.join([x.prettyPrint() for x in varBind])))
+    
+ 
+# Start the tool
+SNMPGet().mainloop()
